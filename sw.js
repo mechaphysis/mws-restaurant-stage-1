02453840dff8936e4a05de0eda2bf77542a9d1dd
mechaphysis <triptofante@gmail.com>
2018-08-01 19:33:19 +0200

    feat: Add basic functionality inside sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,28 @@
 // This is the functionality for the service worker
+
+self.addEventListener('install', function(evt) {
+  evt.waitUntil(
+    caches.open('restaurantsApp').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css7',
+        '/data/restaurants.json',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'img/10.jpg',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        'dbhelper.js'
+      ]);
+    });
+  );
+});
